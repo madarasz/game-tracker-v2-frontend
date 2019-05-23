647489918a32b34ad2220b8c1b01a55f610bf94f
madarasz <mr.madarasz.istvan@gmail.com>
2019-05-23 14:31:30 +0200

    croppie working

--- src/components/ImageUploader.vue
+++ src/components/ImageUploader.vue
@@ -2,14 +2,37 @@
     <div>
         <v-btn @click="launchFilePicker">Upload</v-btn>
         <input type="file" ref="fileInput" style="display:none" @change="onFileChange"/>
-        <img :src="croppieImageSrc" ref="croppieImage" style="max-width: 500px;"/>
-        <div ref="croppieContainer" />
-        <v-btn @click="cropImage" v-show="imageUploaded">Crop</v-btn>
-        <img :src="resultImage" v-show="imageCropped"/>
-        <!-- <v-btn class="btn btn-dark" @click="rotate(+90)">Rotate Left</v-btn>
-        <v-btn class="btn btn-dark" @click="rotate(-90)">Rotate Right</v-btn>
-        <v-btn class="btn btn-dark" @click="crop()">Crop</v-btn> -->
-        <!-- <croppie ref="croppieContainer" @result="result" @update="update" :croppieInitialized="croppieInitialized" :enableOrientation="true" :boundary="{height: 400}"/> -->
+        <!-- Dialog -->
+        <v-dialog v-model="imageUploaded" width="800px" persistent>
+            <v-card>
+                <v-card-title class="headline green lighten-2">
+                    Upload image
+                </v-card-title>
+                <v-card-text>  
+                    <v-layout row wrap fill-height align-center>
+                        <v-flex xs2 text-xs-right>
+                            <v-btn class="btn btn-dark" @click="croppieObject.rotate(+90)" fab>
+                                <v-icon>rotate_left</v-icon>
+                            </v-btn>
+                        </v-flex>
+                        <v-flex xs8>
+                            <div ref="croppieContainer"/>
+                        </v-flex>
+                        <v-flex xs2 text-xs-left>
+                            <v-btn class="btn btn-dark" @click="croppieObject.rotate(-90)" fab>
+                                <v-icon>rotate_right</v-icon>
+                            </v-btn>
+                        </v-flex>
+                    </v-layout>
+                    <img :src="resultImage" v-show="false"/>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn flat @click="resetDialog()">Cancel</v-btn>
+                    <v-btn flat @click="cropImage()">Upload</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </div>
 </template>
 
@@ -23,9 +46,7 @@ export default {
     },
     data () {
         return {
-            croppieImageSrc: null,
             imageUploaded: false,
-            imageCropped: false,
             croppieObject: null,
             resultImage: null
             // outputOptions: {
@@ -62,8 +83,9 @@ export default {
             
             reader.readAsDataURL(imageFile);
             reader.onload = (e) => {
-                // this.croppieImageSrc = e.target.result;
-                this.croppieObject = new Croppie(this.$refs.croppieContainer, options);
+                if (this.croppieObject == null) {
+                    this.croppieObject = new Croppie(this.$refs.croppieContainer, options);
+                }
                 this.croppieObject.bind({ url: e.target.result });
                 this.imageUploaded = true;
             }
@@ -73,54 +95,15 @@ export default {
             this.croppieObject.result('base64').then((dataImg) => {
                 // var data = [{ image: dataImg }, { name: 'myimgage.jpg' }];
                 this.resultImage = dataImg;
-                this.imageCropped = true;
+                // todo send to backend
+                this.resetDialog();
             });
-        }
-        // bind() {
-        //     // // Randomize cat photos, nothing special here.
-        //     // let url = this.images[Math.floor(Math.random() * this.images.length)];
-        //     // // Just like what we did with .bind({...}) on 
-        //     // // the mounted() function above.
-        //     // this.$refs.croppieContainer.bind({
-        //     //     url: url,
-        //     // });
-        // },
-        // // CALLBACK USAGE
-  
-        // crop() {
-        //     // Here we are getting the result via callback function
-        //     // and set the result to this.cropped which is being 
-        //     // used to display the result above.
-        //     this.$refs.croppieContainer.result(this.outputOptions, (output) => {
-        //         this.cropped = output;
-        //     });
-        // },
-        // // EVENT USAGE
-        // cropViaEvent() {
-        //     this.$refs.croppieContainer.result(this.outputOptions);
-        // },
-        // result(output) {
-        //     this.cropped = output;
-        // },
-        // update(val) {
-        //     // eslint-disable-next-line
-        //     console.log(val);
-        // },
-        // rotate(rotationAngle) {
-        // // Rotates the image
-        //     this.$refs.croppieContainer.rotate(rotationAngle);
-        // },
-        // croppieInitialized() {
-        //     // This method will be executed when the croppie is initialized
-        //     // You can use it to set the image
-        //     // this.$refs.croppieRef.bind({
-        //     //   url: 'http://i.imgur.com/Fv2YOM6.jpg',
-        //     // });
-        //     // eslint-disable-next-line
-        //     console.log('Croppie was  initialized');
-        // },
+        },
 
-        
+        resetDialog() {
+            this.imageUploaded = false;
+            this.resultImage = null;
+        }
     }
 };
 
